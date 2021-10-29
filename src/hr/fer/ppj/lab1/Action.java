5d51b2f02a914c789af128641b80586355f87b7f
Marko Barbir <marko.barbir@fer.hr>
2021-10-29 11:35:21 +0200

    Add implemented action class

---
+++ src/hr/fer/ppj/lab1/Action.java
@@ -0,0 +1,24 @@
+package hr.fer.ppj.lab1;
+
+import java.io.Serializable;
+
+public class Action implements Serializable {
+    public String tokenName;
+    public boolean newLine;
+    public String newState;
+    public int goBack;
+
+    public Action(String tokenName){
+        this.tokenName = tokenName;
+        this.newLine = false;
+        this.newState = null;
+        this.goBack = -1;
+    }
+
+    public Action(String tokenName, boolean newLine, String newState, int goBack) {
+        this.tokenName = tokenName;
+        this.newLine = newLine;
+        this.newState = newState;
+        this.goBack = goBack;
+    }
+}
