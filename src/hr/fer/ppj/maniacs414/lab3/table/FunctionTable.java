bf65d53af6dbdd602b52ca7f1423d46aa8c74135
Marko Barbir <marko.barbir@fer.hr>
2021-12-30 18:29:40 +0100

    added type equals

---
+++ src/hr/fer/ppj/maniacs414/lab3/table/FunctionTable.java
@@ -0,0 +1,18 @@
+package hr.fer.ppj.maniacs414.lab3.table;
+
+import hr.fer.ppj.maniacs414.lab3.types.FunctionType;
+import hr.fer.ppj.maniacs414.lab3.types.Type;
+
+import java.util.HashMap;
+
+public class FunctionTable {
+    HashMap<String, FunctionType> functions;
+
+    public FunctionTable() {
+        functions = new HashMap<>();
+    }
+
+    public boolean isAlreadyDeclared(String name) {
+        return functions.containsKey(name);
+    }
+}
